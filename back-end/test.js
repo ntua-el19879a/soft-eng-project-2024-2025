f857c59b6b8e411353f1aa37b7c4510b12b237b6
Dimitris Stathis <el18122@mail.ntua.gr>
2025-02-07 03:23:11 +0200

    Delete back-end/test.js

--- back-end/test.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-
-(async () => {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash("freepasses4all", saltRounds);
-    console.log("Hashed Password:", hashedPassword);
-})();
