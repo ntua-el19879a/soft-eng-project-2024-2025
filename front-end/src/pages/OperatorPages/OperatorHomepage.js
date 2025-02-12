317248cd724119647c848fb724cdba1d11faf231
fotiskoutsikos <fkoutsikos12@gmail.com>
2025-02-12 02:37:34 +0200

    final touches

--- front-end/src/pages/OperatorPages/OperatorHomepage.js
+++ front-end/src/pages/OperatorPages/OperatorHomepage.js
@@ -3,12 +3,8 @@
 import '../../App.css';
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-<<<<<<< HEAD
 import LogoutButton from '../LogoutButton';
 
-=======
-import LogoutButton from '../LogoutButton'; // Import the LogoutButton
->>>>>>> 0d39d326cf4a6b6afce92f5156c9e8ea35b3c1ed
 // Εισαγωγή όλων των operator σελίδων
 import OperatorStatistics from './OperatorStatistics';
 import OperatorFinancials from './OperatorFinancials';
@@ -29,10 +25,6 @@ function OperatorHomepage() {
           </li>
         </ul>
       </nav>
-
-      <div className="logout-container">
-        <LogoutButton />
-      </div>
 
       <Routes>
         <Route path="/operatorstatistics" element={<OperatorStatistics />} />
